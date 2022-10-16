fe7fdb90c2b141cdb8a7f83149b8116f25f1c71c
Chimuanya Nnaji <nnajichimuanya50@gmail.com>
2022-10-16 23:55:37 +0400

    Updated the sites favicon to the hackfest logo

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ export default function Layout({ children, title }) {
           {title ? title + " - hacktoberfest uganda" : "hacktoberfest uganda"}
         </title>
         <meta name="description" content="HacktoberFest Uganda Website" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/hack.jpg" />
       </Head>
       <div className="flex min-h-screen flex-col justify-between">
         <header className="">
